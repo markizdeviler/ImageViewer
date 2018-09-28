91d1bcffadfa9b12a1e73a4278403f7aca47c50c
markizdeviler <muhammadakbar_rafiqov@mail.ru>
2018-09-29 00:13:45 +0500

    merge

--- Viewer/src/main/java/uz/mukhammadakbar/image/viewer/views/ImageViewer.kt
+++ Viewer/src/main/java/uz/mukhammadakbar/image/viewer/views/ImageViewer.kt
@@ -2,7 +2,6 @@ package uz.mukhammadakbar.image.viewer.views
 
 import android.app.Activity
 import android.content.Context
-import android.content.ContextWrapper
 import android.content.Intent
 import android.support.annotation.DrawableRes
 import android.support.v4.content.ContextCompat
@@ -11,8 +10,11 @@ import android.util.AttributeSet
 import android.webkit.URLUtil
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import uz.mukhammadakbar.image.viewer.ui.PictureDetailsActivity
 import uz.mukhammadakbar.image.viewer.utils.Constants
+import uz.mukhammadakbar.image.viewer.ui.PictureDetailsActivity
+import android.content.ContextWrapper
+
+
 
 class ImageViewer: AppCompatImageView {
 
@@ -23,27 +25,6 @@ class ImageViewer: AppCompatImageView {
     constructor(context: Context): super(context){ init() }
 
     constructor(context: Context, attr: AttributeSet): super(context, attr){ init() }
-
-    private fun init() {
-        setOnClickListener {
-            val screenLocation = IntArray(2)
-            getLocationOnScreen(screenLocation)
-            val activity =  scanForActivity(context)
-            val subActivity = Intent(context,
-                    PictureDetailsActivity::class.java)
-            subActivity.putExtra(Constants.ORIENTATION,  resources.configuration.orientation)
-                    .putExtra(Constants.DEFAULT_IMG, defaultImg)
-                    .putExtra(Constants.ERROR_IMG, errorImg)
-                    .putExtra(Constants.IMAGE_URL, imageUrl)
-                    .putExtra(Constants.X_COORD, screenLocation[0] + (this.width - drawable.intrinsicWidth)/2)
-                    .putExtra(Constants.Y_COORD, screenLocation[1] + (this.height - drawable.intrinsicHeight)/2)
-                    .putExtra(Constants.WIDTH, drawable.intrinsicWidth)
-                    .putExtra(Constants.HEIGHT, 2*drawable.intrinsicHeight)
-            activity?.startActivity(subActivity)
-
-            activity?.overridePendingTransition(0, 0)
-        }
-    }
 
     fun imageUrl(url: String){
         imageUrl = url
@@ -68,6 +49,30 @@ class ImageViewer: AppCompatImageView {
         errorImg = drawable
     }
 
+    private fun init() {
+        initListener()
+    }
+
+    private fun initListener() {
+        setOnClickListener {
+            val screenLocation = IntArray(2)
+            getLocationOnScreen(screenLocation)
+            val activity =  scanForActivity(context)
+            val subActivity = Intent(context,
+                    PictureDetailsActivity::class.java)
+            subActivity.putExtra(Constants.ORIENTATION,  resources.configuration.orientation)
+                    .putExtra(Constants.DEFAULT_IMG, defaultImg)
+                    .putExtra(Constants.ERROR_IMG, errorImg)
+                    .putExtra(Constants.IMAGE_URL, imageUrl)
+                    .putExtra(Constants.X_COORD, screenLocation[0] + (this.width - drawable.intrinsicWidth)/2)
+                    .putExtra(Constants.Y_COORD, screenLocation[1] + (this.height - drawable.intrinsicHeight)/2)
+                    .putExtra(Constants.WIDTH, drawable.intrinsicWidth)
+                    .putExtra(Constants.HEIGHT, 2*drawable.intrinsicHeight)
+            activity?.startActivity(subActivity)
+
+            activity?.overridePendingTransition(0, 0)
+        }
+    }
 
     private fun scanForActivity(cont: Context?): Activity? {
         return when (cont) {
